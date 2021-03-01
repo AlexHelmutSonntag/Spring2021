d13f2c67e97ce0578f513448dccc450c01cdfaf1
Ali Magdi <61309161+AlexHelmutSonntag@users.noreply.github.com>
2021-03-01 22:01:47 +0100

    Update polynomial.cpp

--- OOP/Assignment1/polynomial.cpp
+++ OOP/Assignment1/polynomial.cpp
@@ -1,5 +1,5 @@
 #include "polynomial.h"
-#include< iostream>
+#include<iostream>
 using namespace std;
 
 Polynomial::Polynomial()
@@ -7,21 +7,27 @@ Polynomial::Polynomial()
     _degree = 0;
     _vec.resize(0);
 }
-Polynomial::Polynomial(int degree)
+
+Polynomial::Polynomial(int degree,const std::vector<double>& vec)
 {
-    _degree = degree;
-    _vec.resize(_degree);
-}
-void Polynomial:: setCoeff(int degree, double num){
-    if (degree<=0|| degree >_degree){
-        return -1;    
-    }
+    if(vec.size()<=0||degree>vec.size()) throw WRONG_DEGREE;
     else{
-        _vec[degree] = num;
+    _degree = degree;
+    _vec.resize(vec.size());
+    for (int  i = 0; i < vec.size(); i++)
+    {
+        _vec[i]=vec[i];
+    }
+
     }
 }
+
+int Polynomial :: getSize(){
+    return _vec.size();
+}
+
 void Polynomial::print(){
-    
+
     for (int i = _degree; i >=0; i--)
     {
         if(_vec[i]>=0)
@@ -30,33 +36,34 @@ void Polynomial::print(){
         }
         cout<<_vec[i]<<"X^" <<i;
     }
+    cout<<endl;
 }
 
-Polynomial Polynomial::add(Polynomial p1, Polynomial p2){
-    
-    Polynomial return_Poly;
-    /*if(p1._degree == p2._degree)*/
+void Polynomial::add(const Polynomial& p1){
+
+    if(p1._degree!=_degree) throw WRONG_DEGREE;
+    else
     {
         for(int i =p1._degree;i>=0;i--)
         {
-            return_Poly._vec[i]= p1._vec[i]+ p2._vec[i]; 
+            _vec[i]+=p1._vec[i];
         }
     }
-    return return_Poly;
 }
 
 
-Polynomial Polynomial::subtract(Polynomial p1, Polynomial p2){
-    
+void Polynomial::subtract(const Polynomial& p1){
+
     Polynomial return_Poly;
-    // if(p1._degree == p2._degree)
+    if(p1._degree!=_degree) throw WRONG_DEGREE;
+    else
     {
         for(int i =p1._degree;i>=0;i--)
         {
-            return_Poly._vec[i]= p1._vec[i]- p2._vec[i]; 
+            _vec[i]-= p1._vec[i];
         }
     }
-    return return_Poly;
+
 }
 double Polynomial:: evaluate(double x)
 {
@@ -66,6 +73,7 @@ double Polynomial:: evaluate(double x)
         result+=pow(x,i) *_vec[i];
     }
     result+=_vec[0];
-    
+
     return result;
 }
+
