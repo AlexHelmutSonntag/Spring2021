16fa3f73bd064ce0cd03f0c1090aef5c83145d26
Ali Magdi <61309161+AlexHelmutSonntag@users.noreply.github.com>
2021-03-02 14:21:04 +0100

    Add files via upload

---
+++ OOP/polynomials/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include<vector>
+
+#include "Polynomial.h"
+#include "Menu.h"
+
+using namespace std;
+
+int main()
+{
+    Menu test;
+    test.run();
+    /*
+    vector<double> vec1;
+    vec1.push_back(1.0);
+    vec1.push_back(3.0);
+    vec1.push_back(2.0);
+
+
+
+    Polynomial poly1(2,vec1);
+    poly1.print();
+    cout<<poly1.getSize()<<endl;
+
+    vector<double> vec2;
+    vec2.push_back(4.0);
+    vec2.push_back(2.0);
+    vec2.push_back(3.0);
+    Polynomial poly2(2,vec2);
+    poly2.print();
+    cout<<poly2.getSize()<<endl;
+
+
+
+
+    poly1.add(poly2);
+    cout<<"poly1 + poly2 : "<<endl;
+    poly1.print();
+
+    poly1.subtract(poly2);
+    cout<<"(current)poly1 - poly2 : "<<endl;
+    poly1.print();
+
+    cout<<poly1.evaluate(2)<<endl;
+
+
+
+
+    vector<double> vec3{};
+
+    vec3.push_back(-3.0);
+    vec3.push_back(-1.0);
+    vec3.push_back(-4.0);
+
+    Polynomial poly4(3,vec3);
+    cout<<poly4.getSize()<<endl;
+    poly4.print();
+
+*/
+
+
+
+
+
+
+
+    return 0;
+}
