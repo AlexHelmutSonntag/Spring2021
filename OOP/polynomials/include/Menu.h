b70a9316c21d30b28c6bff0fc837398e31589ab4
Ali Magdi <61309161+AlexHelmutSonntag@users.noreply.github.com>
2021-03-02 16:24:41 +0100

    Update Menu.h

--- OOP/polynomials/include/Menu.h
+++ OOP/polynomials/include/Menu.h
@@ -5,19 +5,21 @@
 class Menu
 {
     public:
-    enum EvaluationError{WRONG_INPUT};
+    enum MENU_HANDLING_ERROR{WRONG_INPUT,EMPTY_DATABASE};
 
         Menu() {};
         void run();
     private:
         int printMenu();
         void inputMenu();
+        void printPolys();
         void addMenu();
         void subtractMenu();
         void multiplyMenu();
         void evaluateMenu();
         void print();
         std::vector<Polynomial> database;
+        Polynomial output;
 
 
 };
